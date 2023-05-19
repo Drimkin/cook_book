fce84bbbbd4a6fdedf16cf29549ed35e2071aa14
Dmitry Zavialov <adminptk@mail.ru>
2023-05-19 23:18:06 +0300

    Commit 2

--- recipes.py
+++ recipes.py
@@ -1,19 +1,40 @@
 from pprint import pprint
 
-with open('recipes.txt', 'rt', encoding='utf-8') as f:
+with open('recipes.txt') as file:
     cook_book = {}
-    for line in f:
+    for line in file:
         dish_name = line.strip()
-        ingredient_count = int(f.readline())
+        ingredient_count = int(file.readline())
         dish = []
         for i in range(ingredient_count):
-            book = f.readline()
+            book = file.readline()
             ing, count, unit = book.strip().split(' | ')
             dishes = {
-                'ingredient_name': ing, 'quantity': count, 'measure': unit
+                'ingredient_name': ing,
+                'quantity': count,
+                'measure': unit
             }
             dish.append(dishes)
-        f.readline()
+        file.readline()
         cook_book[dish_name] = dish
-pprint(cook_book)
+    pprint(cook_book, sort_dicts=False)
 
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    for dish in dishes:
+        recipe = cook_book[dish]
+        for ingredient in recipe:
+            name = ingredient['ingredient_name']
+            if name not in shop_list:
+                shop_list_1 = {
+                    'measure': ingredient['measure'],
+                    'quantity': int(ingredient['quantity']) * person_count
+                }
+                shop_list[name] = shop_list_1
+            else:
+                shop_list[name]['quantity'] += int(ingredient['quantity']) * person_count
+
+    pprint(shop_list, sort_dicts=False)
+
+
+get_shop_list_by_dishes(['Омлет', 'Фахитос'], 3)
